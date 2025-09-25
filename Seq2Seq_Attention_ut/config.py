8e7f861d5365020af8ed0ee9aca6667c4bfdb9c4
Ajay Kumar R <152802348+ajay-kumar-r@users.noreply.github.com>
2025-09-25 21:40:54 +0530

    Revert "feat(attention): Implement advanced Seq2Seq Attention model"

--- Seq2Seq_Attention_ut/config.py
+++ Seq2Seq_Attention_ut/config.py
@@ -9,12 +9,14 @@ BATCH_SIZE = 8
 LEARNING_RATE=0.0001
 N_EPOCHS = 10
 CLIP = 1
-BASH_DATASET_PATH = '/Users/mohamedaamir/Documents/MADS/data/bash_dataset.json'
-DOCKER_DATASET_PATH = '/Users/mohamedaamir/Documents/MADS/data/docker_dataset.json'
-GLOVE_PATH = '/Users/mohamedaamir/Documents/MADS/glove.840B.300d.txt'
-TRAIN_SPLIT=0.8
-TEST_SPLIT=0.1
-VALID_SPLIT=0.1
+TRAIN_PATH = '../data/train.csv'
+VALID_PATH = '../data/valid.csv'
+TEST_PATH = '../data/test.csv'
+X_FORMAT='csv'
+GLOVE_PATH = '../glove.840B.300d.txt'
+TRAIN_SPLIT=0.7
+TEST_SPLIT=0.15
+VALID_SPLIT=0.15
 ENCODER_EMBEDDING_DIMENSION=300
 DECODER_EMBEDDING_DIMENSION=300
 LSTM_HIDDEN_DIMENSION = 512
@@ -22,36 +24,7 @@ LSTM_LAYERS=2
 ENCODER_DROPOUT=0.5
 DECODER_DROPOUT=0.5
 LEARNING_RATE=0.001
-MAX_LEN=70
-
-# Advanced Training Parameters
-WARMUP_STEPS = 1000
-LABEL_SMOOTHING = 0.1
-TEACHER_FORCING_RATIO = 0.8
-BEAM_SIZE = 5
-
-# Model save paths
-MODEL_SAVE_FILE='best_model_seq2seq_attention.bin'
-CHECKPOINT_DIR='checkpoints'
-LOG_DIR='logs'
-
-# Results files
-RESULTS_SAVE_FILE='results_attention_final.txt'
-TEST_RESULTS_FILE='test_attention_final.txt'
-TRAINING_HISTORY_FILE='training_history.json'
-
-# Evaluation parameters
-EVAL_EVERY_N_EPOCHS = 3
-SAVE_TOP_K_MODELS = 3
-
-# Early stopping
-EARLY_STOPPING_PATIENCE = 7
-MIN_DELTA = 0.001
-
-# Professional AI settings
-USE_LABEL_SMOOTHING = True
-USE_BEAM_SEARCH = True
-USE_GRADIENT_CLIPPING = True
-USE_LEARNING_RATE_SCHEDULING = True
-USE_EARLY_STOPPING = True
+TEST_MODEL=MODEL_SAVE_FILE='model_seq2seq_1.bin'
+RESULTS_SAVE_FILE='results_1.txt'
+TEST_RESULTS_FILE='test_1.txt'
 
